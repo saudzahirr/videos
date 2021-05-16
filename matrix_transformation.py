205b988df53e3115bdf21b84b7ac23d1233ed42c
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-05-16 22:04:29 +0500

    Update matrix_transformation.py

--- matrix_transformation.py
+++ matrix_transformation.py
@@ -1,10 +1,10 @@
 from manim import *
 
-class LinearTransformation3D(ThreeDScene):
+class MatrixTransformation(ThreeDScene):
     def create_matrix(self, np_matrix):
 
         m = Matrix(np_matrix)
-        m.scale(0.5)
+        m.scale(0.7)
         m.to_corner(UP + LEFT)
 
         return m
@@ -17,6 +17,7 @@ class LinearTransformation3D(ThreeDScene):
         name = Text("aljabrak").to_edge(4*DOWN)
         self.add_fixed_in_frame_mobjects(circle1,title1,name)
         self.play(Create(circle1),Write(title1),Write(name), run_time=5)
+        self.add_sound("3b1b")
         self.wait(2)
 
         circle2 = Circle(radius=0.25,fill_color=BLUE_A,stroke_color=WHITE,stroke_width=1.3,fill_opacity=0.2).to_edge(0.5*DOWN+0.7*RIGHT)
@@ -34,14 +35,6 @@ class LinearTransformation3D(ThreeDScene):
         axes.set_color(GRAY)
         self.set_camera_orientation(phi=75 * DEGREES, theta=-45 * DEGREES)
 
-        #Matrix
-        matrix = self.create_matrix(S)
-        self.add_fixed_in_frame_mobjects(matrix)
-        self.play(
-            FadeIn(matrix),
-            run_time=1,
-        )
-
         #Axes & Camera Motion.
         self.add(axes)
         self.begin_ambient_camera_rotation(rate=0.2)
@@ -49,8 +42,14 @@ class LinearTransformation3D(ThreeDScene):
         cube = Cube(side_length=1, fill_color=BLUE, stroke_width=2, fill_opacity=0.1)
         cube.set_stroke(BLUE)
 
+        #Matrix
+        matrix = self.create_matrix(S)
+        self.add_fixed_in_frame_mobjects(matrix)
+
         self.play(
-            Create(cube)
+            Create(cube),
+            Write(matrix),
+            run_time=1,
         )
 
         self.wait()
@@ -63,16 +62,22 @@ class LinearTransformation3D(ThreeDScene):
 
         self.wait(2)
 
-        exp = Tex(r"This is Shear Transformation","\\\\",
-                    "in 3 Dimensional space"
-        ).to_edge(UP+LEFT)
-        exp[8:27].set_color(BLUE)
-        eq = MathTex("S_{z} = \\begin{pmatrix} 1 & 0 & k_{1} \\\\ 0 & 1 & k_{2} \\\\ 0 & 0 & 1 \\end{pmatrix} ")
-        eq.next_to(exp,DOWN)
-        self.add_fixed_in_frame_mobjects(exp,eq)
+        eq = MathTex("S_{z} = \\begin{bmatrix} 1 & 0 & k_{1} \\\\ 0 & 1 & k_{2} \\\\ 0 & 0 & 1 \\end{bmatrix} ")
+        eq.to_edge(UP+LEFT)
+        self.add_fixed_in_frame_mobjects(eq)
+        exp = Tex(r"This is ","Shear Transformation""\\\\",
+                    " in ","3-Dimensional Space."
+        ).next_to(matrix,DOWN)
+        exp.set_color_by_tex('Shear Transformation',BLUE)
+        exp.set_color_by_tex('3-Dimensional Space.',YELLOW)
+        exp.to_edge(LEFT+4*DOWN)
         self.play(
             Write(exp),
+            run_time=2
+        )
+        self.add_fixed_in_frame_mobjects(exp)
+        self.play(
             ReplacementTransform(matrix,eq),
-            run_time=1,
+            run_time=0.25,
         )
         self.wait(3)
