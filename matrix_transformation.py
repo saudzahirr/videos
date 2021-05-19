742a55ec86b4d24b20a664a333dda95983f5c790
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-05-19 09:41:34 +0500

    Update matrix_transformation.py

--- matrix_transformation.py
+++ matrix_transformation.py
@@ -10,20 +10,7 @@ class MatrixTransformation(ThreeDScene):
         return m
 
     def construct(self):
-
-        #LOGO
-        circle1 = Circle(radius=1,fill_color=BLUE_A,stroke_color=WHITE,stroke_width=1.3,fill_opacity=0.2)
-        title1 = MathTex("\\alpha").scale(4.5)
-        name = Text("aljabrak").to_edge(4*DOWN)
-        self.add_fixed_in_frame_mobjects(circle1,title1,name)
-        self.play(Create(circle1),Write(title1),Write(name), run_time=5)
-        self.add_sound("3b1b")
-        self.wait(2)
-
-        circle2 = Circle(radius=0.25,fill_color=BLUE_A,stroke_color=WHITE,stroke_width=1.3,fill_opacity=0.2).to_edge(0.5*DOWN+0.7*RIGHT)
-        title2 = MathTex("\\alpha").to_edge(0.8*DOWN+0.9*RIGHT)
-        self.play(Transform(circle1,circle2),Transform(title1,title2),FadeOut(name),run_time=1)
-
+        
         #Shear Matrix.
         S = np.array([
             [1, 0, 1],
