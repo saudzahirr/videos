a57fa8baec0f1ed915a1d33be6cea8c62c41a30d
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-09-25 08:20:04 +0500

    Update poem.py

--- short_videos/poem.py
+++ short_videos/poem.py
@@ -6,43 +6,69 @@ https://www.3blue1brown.com/blog/poems
 """
 
 class MathPoetry(Scene):
-    poem = Tex(
-        "When one takes one from one",
-        "plus one from one plus one",
-        "and on and on but ends",
-        "anon then starts again,",
-        "then some sums sum to one,",
-        "to zero other ones.",
-        "One wonders who'd have won",
-        "had stopping not been done;",
-        "had he summed every bit",
-        "until the infinite.",
+    poem = Group(
+        Tex(
+            "When one takes one from one" "\\\\",
+            "plus one from one plus one" "\\\\",
+            "and on and on but ends" "\\\\",
+            "anon then starts again," "\\\\",
+            "then some sums sum to one,"
+        ),
+
+        Tex(
+            "to zero other ones." "\\\\",
+            "One wonders who'd have won" "\\\\",
+            "had stopping not been done;" "\\\\",
+            "had he summed every bit" "\\\\",
+            "until the infinite."
+        ),
         
-        "Lest you should think that such",
-        "less well-known sums are much",
-        "ado about nonsense",
-        "I do give these two cents:",
-        "The universe has got",
-        "an answer which is not",
-        "what most would first surmise,",
-        "it is a compromise,",
-        "and though it seems a laugh",
-        "the universe gives “half”."
+        Tex(
+            "Lest you should think that such" "\\\\",
+            "less well-known sums are much" "\\\\",
+            "ado about nonsense" "\\\\",
+            "I do give these two cents:" "\\\\",
+            "The universe has got"
+        ),
+
+        Tex(
+            "an answer which is not" "\\\\",
+            "what most would first surmise," "\\\\",
+            "it is a compromise," "\\\\",
+            "and though it seems a laugh" "\\\\",
+            "the universe gives “half”."
+        )
     )
     def construct(self):
         logo_transformation(self)
+        self.camera.background_color = GREY_E
         poem = self.poem
-        poem.arrange(DOWN)
-        poem[0].move_to(ORIGIN)
-        poem[1:].move_to(poem[0])
+        speech_bubble = SpeechBubble()
 
-        for line in poem:
-            self.play(
-                Write(line),
-            )
+        self.play(
+            DrawBorderThenFill(speech_bubble)
+        )
+
+        for lines in poem:
+            lines.scale(0.85)
+            lines.move_to(speech_bubble)
+            lines.shift(0.566*UP)
+            for line in lines:
+                self.play(
+                    Write(line),
+                    lag_ratio = 0.05,
+                    run_time = 3,
+                    rate_func = smooth
+                )
+                self.wait(0.75)
+
             self.wait()
             self.play(
-                FadeOut(line, shift = UP)
+                FadeOut(lines),
+                #speech_bubble.animate.flip(UP)
             )
-            
-        self.wait()
+        
+        self.play(
+            FadeOut(speech_bubble)
+        )
+        self.clear()
