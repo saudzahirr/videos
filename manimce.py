b8596db9a387f1017b771bd910c87c437a578ff2
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-10-08 09:15:46 +0500

    Update manimce.py

--- manimce.py
+++ manimce.py
@@ -2,10 +2,11 @@ from manim import *
 from numpy import *
 from scipy import *
 from random import *
+from math import *
 from itertools import *
 from functools import *
 from os import *
-from math import *
+import json
 from custom.logo import *
 from custom.constants import *
 from custom.circuits import Resistor, Source, EarthGround
