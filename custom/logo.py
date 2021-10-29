e5f9e03a128b1f0719bd219fc685bdd51a0d5aaf
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-10-29 17:37:04 +0500

    Update logo.py

--- custom/logo.py
+++ custom/logo.py
@@ -3,6 +3,30 @@ from numpy import *
 from itertools import *
 from constants import *
 
+
+
+def aljabrak(self):
+    #LOGO
+    circle_1 = Circle(radius = 1, fill_color = CHARCOAL, stroke_color = WHITE, stroke_width = 5, fill_opacity = 0.3)
+    title_1 = MathTex("\\alpha")
+    title_1.scale(4.5)
+    logo = VGroup(circle_1, title_1)
+    logo.scale(1.7)
+    logo.shift(0.5 * UP)
+    logo_name = Text("aljabrak")
+    logo_name.scale(1.6)
+    logo_name.next_to(logo, 1.5*DOWN)
+    self.play(
+        LaggedStartMap(Write, logo),
+        Write(logo_name),
+        run_time = 5
+    )
+    self.wait(2)
+    self.play(
+        FadeOut(logo_name, logo)
+    )
+
+    
 def logo(self):
     #LOGO
     circle_1 = Circle(radius=1,fill_color=BLUE_A,stroke_color=WHITE,stroke_width=1.3,fill_opacity=0.2)
@@ -65,3 +89,12 @@ def watermark(self):
     logo.scale(1.2)
     logo.to_edge(0.5*DOWN+0.5*RIGHT)
     self.add(logo)
+
+    
+    
+    
+    
+    
+    
+    
+    
