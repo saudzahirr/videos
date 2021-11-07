eb9c7eacbdfcda77ca9e33df56189d580a983993
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-11-07 13:51:37 +0500

    Small tweaks to SpeechBubble & ThoughtBubble

--- custom/drawings.py
+++ custom/drawings.py
@@ -4,24 +4,168 @@ from manim import *
 
 
 class SpeechBubble(SVGMobject):
-    file_name = "Bubbles_speech.svg"
+    file_name = "bubble-speech.svg"
+    direction = LEFT
+    center_point = ORIGIN
+    content_scale_factor = 0.75
+    bubble_center_adjustment_factor = 1. / 8
+
     def __init__(self, **kwargs):
         SVGMobject.__init__(self, **kwargs)
         self.set_fill(BLACK, 0.8)
         self.set_stroke(WHITE, 3.5)
         self.set_height(5)
         self.set_width(8)
+    
+    def get_tip(self):
+        # TODO, find a better way
+        return self.get_corner(DOWN + self.direction) - 0.6 * self.direction
+
+    def get_bubble_center(self):
+        factor = self.bubble_center_adjustment_factor
+        return self.get_center() + factor * self.get_height() * UP
+
+    def move_tip_to(self, point):
+        mover = VGroup(self)
+        if self.content is not None:
+            mover.add(self.content)
+        mover.shift(point - self.get_tip())
+        return self
+
+    def flip(self, axis=UP):
+        Mobject.flip(self, axis=axis)
+        self.refresh_unit_normal()
+        self.refresh_triangulation()
+        if abs(axis[1]) > 0:
+            self.direction = -np.array(self.direction)
+        return self
+
+    def pin_to(self, mobject):
+        mob_center = mobject.get_center()
+        want_to_flip = np.sign(mob_center[0]) != np.sign(self.direction[0])
+        can_flip = not self.direction_was_specified
+        if want_to_flip and can_flip:
+            self.flip()
+        boundary_point = mobject.get_bounding_box_point(UP - self.direction)
+        vector_from_center = 1.0 * (boundary_point - mob_center)
+        self.move_tip_to(mob_center + vector_from_center)
+        return self
+
+    def position_mobject_inside(self, mobject):
+        scaled_width = self.content_scale_factor * self.get_width()
+        if mobject.get_width() > scaled_width:
+            mobject.set_width(scaled_width)
+        mobject.shift(
+            self.get_bubble_center() - mobject.get_center()
+        )
+        return mobject
+
+    def add_content(self, mobject):
+        self.position_mobject_inside(mobject)
+        self.content = mobject
+        return self.content
+
+    def write(self, *text):
+        self.add_content(TexText(*text))
+        return self
+
+    def resize_to_content(self):
+        target_width = self.content.get_width()
+        target_width += max(MED_LARGE_BUFF, 2)
+        target_height = self.content.get_height()
+        target_height += 2.5 * LARGE_BUFF
+        tip_point = self.get_tip()
+        self.stretch_to_fit_width(target_width)
+        self.stretch_to_fit_height(target_height)
+        self.move_tip_to(tip_point)
+        self.position_mobject_inside(self.content)
+
+    def clear(self):
+        self.add_content(VMobject())
+        return self
 
 
 
 class ThoughtBubble(SVGMobject):
-    file_name = "Bubbles_thought.svg"
+    file_name = "bubble-thought.svg"
+    direction = LEFT
+    center_point = ORIGIN
+    content_scale_factor = 0.75
+    bubble_center_adjustment_factor = 1. / 8
+
     def __init__(self, **kwargs):
         SVGMobject.__init__(self, **kwargs)
         self.set_fill(BLACK, 0.8)
         self.set_stroke(WHITE, 3.5)
         self.set_height(5)
         self.set_width(8)
+    
+    def get_tip(self):
+        # TODO, find a better way
+        return self.get_corner(DOWN + self.direction) - 0.6 * self.direction
+
+    def get_bubble_center(self):
+        factor = self.bubble_center_adjustment_factor
+        return self.get_center() + factor * self.get_height() * UP
+
+    def move_tip_to(self, point):
+        mover = VGroup(self)
+        if self.content is not None:
+            mover.add(self.content)
+        mover.shift(point - self.get_tip())
+        return self
+
+    def flip(self, axis=UP):
+        Mobject.flip(self, axis=axis)
+        self.refresh_unit_normal()
+        self.refresh_triangulation()
+        if abs(axis[1]) > 0:
+            self.direction = -np.array(self.direction)
+        return self
+
+    def pin_to(self, mobject):
+        mob_center = mobject.get_center()
+        want_to_flip = np.sign(mob_center[0]) != np.sign(self.direction[0])
+        can_flip = not self.direction_was_specified
+        if want_to_flip and can_flip:
+            self.flip()
+        boundary_point = mobject.get_bounding_box_point(UP - self.direction)
+        vector_from_center = 1.0 * (boundary_point - mob_center)
+        self.move_tip_to(mob_center + vector_from_center)
+        return self
+
+    def position_mobject_inside(self, mobject):
+        scaled_width = self.content_scale_factor * self.get_width()
+        if mobject.get_width() > scaled_width:
+            mobject.set_width(scaled_width)
+        mobject.shift(
+            self.get_bubble_center() - mobject.get_center()
+        )
+        return mobject
+
+    def add_content(self, mobject):
+        self.position_mobject_inside(mobject)
+        self.content = mobject
+        return self.content
+
+    def write(self, *text):
+        self.add_content(TexText(*text))
+        return self
+
+    def resize_to_content(self):
+        target_width = self.content.get_width()
+        target_width += max(MED_LARGE_BUFF, 2)
+        target_height = self.content.get_height()
+        target_height += 2.5 * LARGE_BUFF
+        tip_point = self.get_tip()
+        self.stretch_to_fit_width(target_width)
+        self.stretch_to_fit_height(target_height)
+        self.move_tip_to(tip_point)
+        self.position_mobject_inside(self.content)
+
+    def clear(self):
+        self.add_content(VMobject())
+        return self
        
     
     
@@ -157,3 +301,9 @@ class Spybot(SVGMobject):
         
         
         
+"""
+The functions in SpeechBubble & ThoughtBubble
+are originally made by 3Blue1Brown, I've made
+some modifications in it so that one can use
+them in Community Version Manim.
+"""
