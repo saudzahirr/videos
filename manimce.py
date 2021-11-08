355d389c72c1cfb8ed422a6b09dfce2c3cfbda5b
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-11-08 23:02:57 +0500

    Update manimce.py

--- manimce.py
+++ manimce.py
@@ -12,7 +12,6 @@ from custom.constants import *
 from custom.circuits import Resistor, Source, EarthGround
 from custom.drawings import SpeechBubble, ThoughtBubble
 from custom.characters.sigma_creature import sigma_creature, blink_eye
-from custom.functions import fourier_series, quaternion_multiplication
 
 
 
