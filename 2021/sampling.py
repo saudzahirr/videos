dc2a12b25417ba57a33874512e0f37636f2cd455
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-12-08 00:12:04 +0500

    Update sampling.py

--- 2021/sampling.py
+++ 2021/sampling.py
@@ -1,1 +1,44 @@
 from manimce import *
+from custom.functions import fourier_series, square_wave
+from custom.drawings import Speaker
+
+
+
+def get_sampling(function, x_min, x_max, sample_step):
+    """Samples a continuous function into discrete
+    function and returns discrete plot."""
+    x = [i for i in arange(x_min, x_max, sample_step)]
+    y = [function(i) for i in arange(x_min, x_max, sample_step)]
+
+    f = []
+    for m, n in zip(x, y):
+        q = [m, n, 0]
+        f.append(q)
+
+    points = VGroup()
+    for coordinates in f:
+        points.add(Dot(coordinates))
+
+    stem_plot = VGroup()
+    stem_plot.add(points)
+
+    x_coords = []
+    for i in x:
+        x = [i, 0, 0]
+        x_coords.append(x)
+
+    for x_coord, y_coord in zip(x_coords, f):
+        line = Line(x_coord, y_coord)
+        stem_plot.add(line)
+
+    return stem_plot
+
+
+
+def get_graph(function, x_min, x_max):
+    graph = FunctionGraph( 
+        lambda x : function(x),
+        x_range = [x_min, x_max],
+        color = BLUE
+    )
+    return graph
