3ff8f9ebee52620037e85cd0624be9c668614988
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-12-08 00:23:35 +0500

    Import get_sampling from sampling.py

--- 2022/nyquist_shannon_theorem.py
+++ 2022/nyquist_shannon_theorem.py
@@ -1,4 +1,8 @@
 from manimce import *
+from custom.functions import fourier_series, square_wave
+from custom.drawings import Speaker
+from 2021.sampling import get_sampling
+
 
 
 class Introduction(Scene):
