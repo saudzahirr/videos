28f01bf3b42bd9be165f113c288b45e3c2f41827
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-12-08 00:49:05 +0500

    Update nyquist_shannon_theorem.py

--- 2022/nyquist_shannon_theorem.py
+++ 2022/nyquist_shannon_theorem.py
@@ -1,7 +1,7 @@
 from manimce import *
 from custom.functions import fourier_series, square_wave
 from custom.drawings import Speaker
-from 2021.sampling import get_sampling
+
 
 
 class Introduction(Scene):
@@ -10,10 +10,3 @@ class Introduction(Scene):
 
 class NyquistShannonTheorem(Scene):
   pass
-
-
-
-
-
-
-
