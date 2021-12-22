a9a3c125d2ae17860dbcab31f6d1a4a884bb7f40
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-12-22 15:21:55 +0500

    v1.3.0

---
+++ manim_ext.py
@@ -0,0 +1,11 @@
+from manimlib import *
+from numpy import *
+from scipy import *
+from random import *
+from math import *
+from itertools import *
+from functools import *
+from os import *
+import json
+from custom.logo import *
+from custom.constants import *
