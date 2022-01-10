1f0b98c5fad3740158df21828d1af2d5afe22510
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2022-01-10 22:04:51 +0500

    `Manim Community v0.14.0`

--- manimce.py
+++ manimce.py
@@ -7,8 +7,9 @@ from itertools import *
 from functools import *
 from os import *
 import json
-from custom.logo_ import *
+from custom.logo import *
 from custom.constants import *
+from custom.functions import *
 from custom.circuits import Resistor, Source, EarthGround
 from custom.drawings import SpeechBubble, ThoughtBubble
 from custom.characters.sigma_creature import sigma_creature, blink_eye
