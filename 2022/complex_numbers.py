8461baaac1fefdb826b4c0f8328ba435435f473c
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2022-01-17 14:48:37 +0500

    Update complex_numbers.py

--- 2022/complex_numbers.py
+++ 2022/complex_numbers.py
@@ -4,15 +4,10 @@ from manimce import *
 
 class CasusIrreducibilis(Scene):
     def construct(self):
+        kwargs = {"run_time" : 3, "rate_func" : bezier([0, 0, 1, 1])}
         year = Tex("1545")
         year.scale(1.5)
         year.to_edge(UP + 2 * LEFT)
-        italy_map = SVGMobject(get_svg("Italy.svg"))
-        italy_map.scale(1.5)
-        italy_map.next_to(year, RIGHT, buff = 1.0)
-
-        for part, color in zip(italy_map, cycle([GREEN, WHITE, RED])):
-            part.set_color(color)
         
         equations = VGroup(
             MathTex("x^{2} + 2x + 2 = 0"),
@@ -28,8 +23,6 @@ class CasusIrreducibilis(Scene):
 
         self.play(
             Write(year),
-            DrawBorderThenFill(italy_map),
-            run_time = 2,
             rate_func = smooth
         )
         for equation, root in zip(equations, roots):
@@ -47,21 +40,23 @@ class CasusIrreducibilis(Scene):
         self.wait(2)
 
         self.play(
-            FadeOut(year, italy_map, equations, roots[0]),
-            roots[1].animate.move_to(ORIGIN)
+            FadeOut(year, equations, roots[0]),
+            roots[1].animate.next_to(ORIGIN, 0.25 * RIGHT)
         )
         self.wait()
 
 
         casus_irreducibilis = Tex(r"Casus Irreducibilis")
         casus_irreducibilis.scale(1.2)
-        casus_irreducibilis.next_to(roots[1], UP, buff = 1.0)
-        translation = Tex(r"Latin for “the irreducible case”")
+        casus_irreducibilis.next_to(roots[1], UP, buff = 1.5)
+        translation = Tex("Latin for ", "“the irreducible case”")
+        translation.set_color_by_tex("“the irreducible case”", YELLOW)
         translation.set_color(GREY_B)
         translation.next_to(casus_irreducibilis, DOWN, buff = 0.25)
         bombelli = ImageMobject(get_portrait("Rafael_Bombelli.jpeg"))
         bombelli.scale(1.5)
         bombelli.to_edge(LEFT)
+        bombelli.shift(DOWN)
         rafael_bombelli = Tex(r"Rafael Bombelli")
         rafael_bombelli.next_to(bombelli, DOWN)
 
@@ -76,9 +71,45 @@ class CasusIrreducibilis(Scene):
         self.wait(2)
 
         self.play(
-            FadeIn(bombelli),
-            Write(rafael_bombelli),
+            FadeIn(bombelli, rafael_bombelli),
             rate_func = smooth,
             run_time = 2
+        )
+        self.wait()
+
+
+        speech = SpeechBubble()
+        speech.next_to(bombelli.get_right(), UP)
+        words = Tex(
+            "Plus by plus of minus," "\\\\",
+            "makes plus of minus." "\\\\",
+            "Minus by plus of minus," "\\\\",
+            "makes minus of minus." "\\\\"
+        )
+        speech.add_content(words)
+        speech.position_mobject_inside(words)
+        speech.resize_to_content()
+        speech.move_tip_to(bombelli.get_right())
+
+
+        self.play(
+            Create(speech),
+            Write(words, run_time = 3, rate_func = smooth),
+            rate_func = smooth
+        )
+        self.wait(2)
+
+
+        bombelli_rules = Tex(
+            "“plus of minus” for $i = \\sqrt{-1}$" "\\\\",
+            "“minus of minus” for $-i = -\\sqrt{-1}$",
+        )
+        bombelli_rules.set_color(YELLOW)
+        bombelli_rules.next_to(roots[1], DOWN, buff = 1.0)
+
+        self.play(
+            Write(bombelli_rules),
+            rate_func = smooth,
+            run_time = 3
         )
         self.wait()
