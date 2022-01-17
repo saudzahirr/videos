25a7e12ce453607d0f9f991956bb3ff29b0d0f7b
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2022-01-17 07:01:56 +0500

    Update complex_numbers.py

--- 2022/complex_numbers.py
+++ 2022/complex_numbers.py
@@ -1,2 +1,11 @@
 from manimce import *
 
+
+
+class Introduction(Scene):
+    def construct(self):
+        euler = ImageMobject(get_portrait("Leonhard_Euler"))
+        self.play(
+            FadeIn(euler)
+        )
+        self.wait()
