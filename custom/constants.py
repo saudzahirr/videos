89baa53c3b44015e89defd537f57e8b0edcaf7f6
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2022-01-18 14:29:53 +0500

    Update constants.py

--- custom/constants.py
+++ custom/constants.py
@@ -1,4 +1,4 @@
-# Constants.
+# Frame and Pixel shape.
 ASPECT_RATIO = 16.0 / 9.0
 FRAME_HEIGHT = 8.0
 FRAME_WIDTH = ASPECT_RATIO * FRAME_HEIGHT
@@ -9,11 +9,42 @@ DEFAULT_PIXEL_HEIGHT = 1080
 DEFAULT_PIXEL_WIDTH = 1920
 DEFAULT_FRAME_RATE = 30
 
+# Buffs.
 SMALL_BUFF = 0.1
 MED_SMALL_BUFF = 0.25
 MED_LARGE_BUFF = 0.5
 LARGE_BUFF = 1
 
+# Coordinates.
+ORIGIN = np.array((0., 0., 0.))
+UP = np.array((0., 1., 0.))
+DOWN = np.array((0., -1., 0.))
+RIGHT = np.array((1., 0., 0.))
+LEFT = np.array((-1., 0., 0.))
+IN = np.array((0., 0., -1.))
+OUT = np.array((0., 0., 1.))
+X_AXIS = np.array((1., 0., 0.))
+Y_AXIS = np.array((0., 1., 0.))
+Z_AXIS = np.array((0., 0., 1.))
+
+# Useful abbreviations for diagonals
+UL = UP + LEFT
+UR = UP + RIGHT
+DL = DOWN + LEFT
+DR = DOWN + RIGHT
+
+TOP = FRAME_Y_RADIUS * UP
+BOTTOM = FRAME_Y_RADIUS * DOWN
+LEFT_SIDE = FRAME_X_RADIUS * LEFT
+RIGHT_SIDE = FRAME_X_RADIUS * RIGHT
+
+# Texts
+START_X = 30
+START_Y = 20
+NORMAL = "NORMAL"
+ITALIC = "ITALIC"
+OBLIQUE = "OBLIQUE"
+BOLD = "BOLD"
 
 # Extra Colors.
 CHAR_TREE = "#7FFF00"
