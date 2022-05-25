85c5d676856563fef86cebcaa1a983b6dc774bac
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2022-05-26 01:58:32 +0500

    Changed Tex to MathTex

--- outside_videos/fibonacci_mapping.py
+++ outside_videos/fibonacci_mapping.py
@@ -20,25 +20,42 @@ class FibonacciMapping(Scene):
         )
         plane.scale(2)
 
-        formula = Tex(
-            "$F_{n} = {\\varphi^{n} - \\left( - \\varphi \\right)^{n}} \\over \\sqrt{5}$"
+        kw = {"tex_to_color_map": {
+            "F": BLUE_C,
+            "{n}": BLUE_B,
+            "{\\varphi}": YELLOW}
+        }
+
+        formula = MathTex(
+            "F_{n} = { { {\\varphi}^{n} - \\left( - {\\varphi} \\right)^{n} } \\over 2 {\\varphi} - 1 }",
+            **kw
         )
         formula.scale(1.25)
         formula.to_corner(UL)
         formula.set_stroke(BLACK, 5, background = True)
-        formula.add_background_rectangle()
+
+        golden_ratio = MathTex(
+            "{\\varphi} = { {1 + \\sqrt{5} } \\over 2 }",
+            **kw
+        )
+        golden_ratio.next_to(formula.get_bottom(), DOWN)
+        golden_ratio.set_stroke(BLACK, 5, background = True)
+        formula.add(golden_ratio)
+        
+        rect = BackgroundRectangle(formula, fill_opacity = 0.5, buff = SMALL_BUFF)
 
         grid = plane.copy()
         grid.set_stroke(GREY_B, 2)
         grid.prepare_for_nonlinear_transform()
-        self.add(grid)
+        self.add(grid, rect)
 
-        plane.add_coordinates(font_size = 20)
+        plane.add_coordinates(font_size = 30)
         self.add_foreground_mobjects(plane, formula)
+        self.wait(2)
 
         self.play(
             grid.animate.apply_complex_function(lambda z: F(z)),
             rate_func = smooth,
-            run_time = 6,
+            run_time = 10,
         )
-        self.wait()
+        self.wait(2)
