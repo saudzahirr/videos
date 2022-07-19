6c8d6f2cc9d1eee7a28df8829e46f1b9cdd709d1
Saud Zahir <mzahir.bee19seecs@seecs.edu.pk>
2022-07-19 14:05:26 +0500

    Update deprecated.py

--- custom/deprecated.py
+++ custom/deprecated.py
@@ -1,6 +1,6 @@
 from manim import *
 
-# Not useful.
+# This file contains deprecated classes and functions.
 
 class Checkmark(Tex):
     def __init__(self, **kwargs):
@@ -19,3 +19,21 @@ class Exmark(Tex):
         super().__init__("\\exmark")
         self.scale(2.5)
         self.set_color(RED)
+
+        
+
+class FadeInFromDown(FadeIn):
+    def __init__(self, mobject, **kwargs):
+        super().__init__(mobject, UP, **kwargs)
+
+        
+
+class FadeOutAndShiftDown(FadeOut):
+    def __init__(self, mobject, **kwargs):
+        super().__init__(mobject, DOWN, **kwargs)
+
+
+        
+class FadeInFromLarge(FadeIn):
+    def __init__(self, mobject, scale_factor=2, **kwargs):
+        super().__init__(mobject, scale=(1 / scale_factor), **kwargs)
